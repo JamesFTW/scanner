b5d643d8650a42110cda566fad445941d17d858a
James Andrews <bdaddyja@gmail.com>
2018-10-07 10:02:56 -0700

    Added backgroundComponent

---
+++ scanner/app/components/backGroundComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+export default class backGround extends Component {
+
+  render() {
+    return (
+      <ScrollView style={styles.backGround}/>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  backGround: {
+    display: 'flex',
+    backgroundColor: '#F2F2F2',
+    flex: 1
+  }
+})
