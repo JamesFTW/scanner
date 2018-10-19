d6f99122d06219c0bf8dbb9c038bf726e47ab1f4
James Andrews <bdaddyja@gmail.com>
2018-10-18 17:29:23 -0700

    Bare bones server stuff.

---
+++ scanner/server.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+const pg      = require('pg')
+const pgp     = require("pg-promise")()
+const db      = pgp(process.env.DATABASE_URL)
+const helmet  = require('helmet')
+
+const app  = express()
+const port = process.env.PORT || 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World! \n' + process.env.DATABASE_URL)
+})
+
+app.listen(port, () => `Listening on port ${port}`)
