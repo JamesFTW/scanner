736751327d7a715f397931f90c052c2f49cf0330
James Andrews <bdaddyja@gmail.com>
2018-10-20 04:08:54 -0700

    Added migration.

---
+++ migrations/20181020040607-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(`Users`, {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      profilePhoto: {
+        type: Sequelize.STRING,
+        allowNull: true
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(`Users`);
+  }
+};
