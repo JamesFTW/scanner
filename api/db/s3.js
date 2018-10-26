8e09355d0b5257b81606f15dfc918bf0a50a4af3
James Andrews <bdaddyja@gmail.com>
2018-10-25 20:54:11 -0700

    Added S3 to project.

---
+++ api/db/s3.js
@@ -0,0 +1,39 @@
+
+ if(process.env.NODE_ENV === 'development') {
+   require('dotenv').config()
+ }
+
+const accessKey = process.env.AWSAccessKeyId
+const secretKey = process.env.AWSSecretKey
+
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
+
+const creds = new aws.Credentials(accessKey, secretKey, null)
+
+aws.config.update({
+  credentials: creds,
+  secretAccessKey: accessKey,
+  accessKeyId: secretKey,
+  region: 'us-west-1'
+})
+
+const s3 = new aws.S3()
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'scanner-profilephotos',
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname})
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+module.exports = upload
