e3b516f3b4837d681298292bcadb82486ccb468a
James Andrews <bdaddyja@gmail.com>
2018-10-25 22:23:42 -0700

    Move S3 to index.js

--- api/db/index.js
+++ api/db/index.js
@@ -6,7 +6,11 @@ if(process.env.NODE_ENV === 'development') {
 const pg  = require('pg')
 const pgp = require("pg-promise")()
 const db  = pgp(process.env.DATABASE_URL)
+const s3  = require('./s3.js')
 
 pg.defaults.ssl = true
 
-module.exports = db;
+module.exports = {
+  db : db,
+  s3 : s3
+}
