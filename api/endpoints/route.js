98edf3df48af24eecd2441ef1c89f15c6fdeaef3
James Andrews <bdaddyja@gmail.com>
2018-10-25 20:54:53 -0700

    Added S3 to routes.

--- api/endpoints/route.js
+++ api/endpoints/route.js
@@ -3,9 +3,15 @@ const express = require("express")
 const router  = express.Router()
 
 const db = require('../db')
+const s3 = require('../db/s3.js')
 
 router.get('/', (req, res) => {
   res.send('Hello World! \n' + process.env.DATABASE_URL)
+})
+
+router.get('/test', s3.single('avatar'), (req, res, next) => {
+  //this route will be used for posting profile photos
+  return res.json({'imageUrl': req.file})
 })
 
 module.exports = router
