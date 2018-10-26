23efc3e7858a0f4a52838ec9f146507ca3adf1e3
James Andrews <bdaddyja@gmail.com>
2018-10-25 22:24:07 -0700

    Remove s3 require.

--- api/endpoints/route.js
+++ api/endpoints/route.js
@@ -3,13 +3,12 @@ const express = require("express")
 const router  = express.Router()
 
 const db = require('../db')
-const s3 = require('../db/s3.js')
 
 router.get('/', (req, res) => {
   res.send('Hello World! \n' + process.env.DATABASE_URL)
 })
 
-router.post('/test', s3.single('avatar'), (req, res, next) => {
+router.post('/test', db.s3.single('avatar'), (req, res, next) => {
   //this route will be used for posting profile photos
   return res.json({'imageUrl': req.file})
 })
