e7d73d837cb192f926b9dca418d0382ecb3de6b5
James Andrews <bdaddyja@gmail.com>
2018-10-25 20:55:12 -0700

    Removed route from server.js

--- api/server.js
+++ api/server.js
@@ -10,8 +10,4 @@ const router  = require('./endpoints/route.js')
 app.use(helmet())
 app.use('/', router)
 
-app.get('/test', (req, res) => {
-  console.log(req.socket)
-})
-
 app.listen(port, () => console.log(`Listening on port ${port}`))
