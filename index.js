0ea59bf9b8c93b326bf384a1872e6406c043064c
James Andrews <bdaddyja@gmail.com>
2018-11-04 00:58:50 -0700

    Formatting index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import ProfileScreen from './app/screens/ProfileScreen'
 
-AppRegistry.registerComponent('scanner', () => ProfileScreen);
+import { AppRegistry } from 'react-native';
+import App             from './App';
+import ProfileScreen   from './app/screens/ProfileScreen'
+import Camera          from './app/components/cameraComponent.js'
+
+AppRegistry.registerComponent('scanner', () => Camera);
