b6880c7b916505fde6c465ddf525bacd212c3d7b
James Andrews <bdaddyja@gmail.com>
2018-11-19 22:56:51 -0800

    Move dependencies around.

--- api/db/index.js
+++ api/db/index.js
@@ -5,8 +5,8 @@ if(process.env.NODE_ENV === 'development') {
 
 const pg  = require('pg')
 const pgp = require("pg-promise")()
-const db  = pgp(process.env.DATABASE_URL)
 const s3  = require('./s3.js')
+const db  = pgp(process.env.DATABASE_URL)
 
 pg.defaults.ssl = true
 
