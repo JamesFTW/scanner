25165c0074a990cafe940a6e44f471b198194dbc
James Andrews <bdaddyja@gmail.com>
2018-11-21 23:38:23 -0800

    Changed name of endpoint.

--- api/endpoints/route.js
+++ api/endpoints/route.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   res.send('Hello World! \n' + process.env.DATABASE_URL)
 })
 
-router.post('/test', db.s3.single('avatar'), (req, res, next) => {
+router.post('/avatarUpload', db.s3.single('avatar'), (req, res, next) => {
   //this route will be used for posting profile photos
   return res.json({'imageUrl': req.file})
 })
