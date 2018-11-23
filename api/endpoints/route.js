afe4284c9b05c312721bf389d2fc565c5ef93d06
James Andrews <bdaddyja@gmail.com>
2018-11-22 18:59:06 -0800

    Object destructoring.

--- api/endpoints/route.js
+++ api/endpoints/route.js
@@ -2,13 +2,13 @@
 const express = require("express")
 const router  = express.Router()
 
-const db = require('../db')
+const { s3 } = require('../db')
 
 router.get('/', (req, res) => {
   res.send('Hello World! \n' + process.env.DATABASE_URL)
 })
 
-router.post('/avatarUpload', db.s3.single('avatar'), (req, res, next) => {
+router.post('/avatarUpload', s3.single('avatar'), (req, res, next) => {
   //this route will be used for posting profile photos
   return res.json({'imageUrl': req.file})
 })
