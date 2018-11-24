0bb18978602d21a9c7d9b5c71fcda174dc30b8ce
James Andrews <bdaddyja@gmail.com>
2018-11-24 14:18:07 -0800

    Remove app from index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 import { AppRegistry } from 'react-native';
-import App             from './App';
 import ProfileScreen   from './app/screens/ProfileScreen'
 import Camera          from './app/components/cameraComponent.js'
 
