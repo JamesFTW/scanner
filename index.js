2f5b5b57908610d69fbd3e750f18525e93997cf4
James Andrews <bdaddyja@gmail.com>
2018-11-24 22:50:46 -0800

    Changed import.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { AppRegistry } from 'react-native';
 import ProfileScreen   from './app/screens/ProfileScreen'
-import Camera          from './app/components/cameraComponent.js'
+import Camera          from './app/containers/cameraComponent.js'
 
 AppRegistry.registerComponent('scanner', () => Camera);
