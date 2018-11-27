f0d42707328b612fc68cd2c85da276cd8fbe4d0c
James Andrews <bdaddyja@gmail.com>
2018-11-26 17:13:30 -0800

    WIP ConfirmationScreen.

---
+++ app/screens/ConfirmationScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import TitleBackground from '../components/titleBackGround.js'
+import PaperComponent  from '../components/paperComponent.js'
+import TitleComponent  from '../components/titleComponent.js'
+
+export default class ConfirmationScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Select Food Item"
+    }
+  }
+
+  render() {
+    return (
+    <ScrollView contentContainerStyle={{flex: 1}}>
+      <TitleComponent title={this.state.title}/>
+      <TitleBackground>
+        <PaperComponent/>
+      </TitleBackground>
+    </ScrollView>
+    )
+  }
+}
