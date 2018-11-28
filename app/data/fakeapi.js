405e68726fd1be936fc6a751108f9de879e66e2f
James Andrews <bdaddyja@gmail.com>
2018-11-27 16:45:34 -0800

    Added fake api data directory.

---
+++ app/data/fakeapi.js
@@ -0,0 +1,22 @@
+export const payLoad = [
+  {
+  order: "Bacon Burger",
+  price: 2.00,
+  quanity: 1
+  },
+  {
+  order: "Fries",
+  price: 1.23,
+  quanity: 1
+  },
+  {
+  order: "Cheese Burger",
+  price: 1.10,
+  quanity: 1
+  },
+  {
+  order: "Pizza",
+  price: 1.23,
+  quanity: 1
+  }
+]
