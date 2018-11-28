a203de6993f2bd61d549011df0109af6f0ac428c
James Andrews <bdaddyja@gmail.com>
2018-11-27 16:47:30 -0800

    Testing Confirmation Screen.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import { AppRegistry }    from 'react-native';
 import ProfileScreen      from './app/screens/ProfileScreen'
 import ConfirmationScreen from './app/screens/ConfirmationScreen'
 import TitleComponent     from './app/components/titleComponent.js'
+import Camera             from './app/containers/cameraComponent.js'
 
-AppRegistry.registerComponent('scanner', () => ConfirmationScreen);
+AppRegistry.registerComponent('scanner', () => ConfirmationScreen)
