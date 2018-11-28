90ea2b3f62ebbedf9a52d946abf519861da9d365
James Andrews <bdaddyja@gmail.com>
2018-11-27 16:47:17 -0800

    Note for server api.

--- api/server.js
+++ api/server.js
@@ -10,4 +10,7 @@ const router  = require('./endpoints/route.js')
 app.use(helmet())
 app.use('/', router)
 
+//I think for the api I can still set up a proxy for local dev, but for
+//prod have to user http://${APIurl}
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
