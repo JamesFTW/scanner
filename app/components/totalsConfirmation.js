9cb8519ab85c1468014f0ce70872e6cb29b65459
James Andrews <bdaddyja@gmail.com>
2018-12-18 20:55:37 -0800

    Basics for totalsConfirmation done.

---
+++ app/components/totalsConfirmation.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  View
+} from 'react-native'
+
+const checkmark = require('../img/checkmark/Yes.png')
+const x         = require('../img/x/Group.png')
+
+export default class TotalConfirmation extends Component {
+  render() {
+    return (
+      <View style={styles.backGround}>
+        <View style={styles.title}>
+          <Text style={styles.text}>Is this amount correct?</Text>
+        </View>
+        <View style={styles.flexButtons}>
+          <View style={styles.line}></View>
+            <Image style={styles.check} source={x} />
+          <Text style={styles.no}>No</Text>
+          <View style={styles.line}></View>
+            <Image style={styles.check} source={checkmark} />
+          <Text style={styles.yes}>Yes</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  flexButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '50%'
+  },
+  line: {
+    borderRightWidth: 1,
+    borderRightColor: '#BDBDBD'
+  },
+  no : {
+    fontFamily: 'SanFranciscoText-Bold',
+    color: '#EB5757',
+    fontSize: 18,
+    marginTop: 10,
+    paddingRight: '24%',
+    marginLeft: 50
+  },
+  yes: {
+    fontFamily: 'SanFranciscoText-Bold',
+    color: '#6FCF97',
+    fontSize: 18,
+    marginTop: 10,
+    marginLeft: 50
+  },
+  check: {
+    marginLeft: 10,
+    marginTop: 10
+  },
+  backGround: {
+    width: '100%',
+    height: 98.01,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 10,
+    display: 'flex',
+    top: 50,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2
+  },
+  title: {
+    marginTop: 10,
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+    borderBottomColor: '#BDBDBD'
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#4F4F4F',
+    fontFamily: 'SanFranciscoText-SemiBold',
+    fontSize: 24,
+    borderBottomColor: '#4F4F4F'
+  }
+})
