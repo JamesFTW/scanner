f5418b9f0d411c7a74252603677e1cdc68f8074e
James Andrews <bdaddyja@gmail.com>
2018-12-20 14:25:21 -0800

    Add state to tipScreen.

--- app/screens/TipScreen.js
+++ app/screens/TipScreen.js
@@ -13,15 +13,38 @@ import TitleComponent  from '../components/titleComponent.js'
 import TitleBackground from '../components/titleBackGround.js'
 
 export default class TipScreen extends Component {
-  testPress = () => {
-    console.log(this)
+  constructor() {
+    super()
+    this.state = {
+      tipPercent: 0
+    }
+  }
+  
+  onTip = (tip) => {
+    switch (tip) {
+      case '0':
+      this.setState({tipPercent: 'No Tip'})
+        break
+      case '1':
+      this.setState({tipPercent: .1})
+        break
+      case '2':
+      this.setState({tipPercent: .15})
+        break
+      case '3':
+      this.setState({tipPercent: .2})
+      break
+      case '4':
+      this.setState({tipPercent: 'Other'})
+        break
+    }
   }
   render() {
     return(
       <TitleBackground>
         <TitleComponent title={"Select Tip Amount"}/>
           <View style={styles.tipContainer}>
-            <TipComponent tip={this.testPress} />
+            <TipComponent onTip={this.onTip} />
           </View>
       </TitleBackground>
     )
